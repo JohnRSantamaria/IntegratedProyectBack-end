e20f905901d6dada84251c337495b1d425a0ed48
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-03-24 19:52:56 -0500

    deploy

--- index.js
+++ index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT =  process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s raised in port ${PORT}`); // eslint-disable-line no-console
   });
 });
