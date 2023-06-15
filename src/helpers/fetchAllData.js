d1246b36c7ca0f1d6d36f00582b70ea6e99036a8
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-15 15:50:32 -0500

    cambiando el numero de recetas

--- src/helpers/fetchAllData.js
+++ src/helpers/fetchAllData.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {API_KEY, API_KEY2, API_KEY3, API_KEY4 } = process.env;
+const { API_KEY, API_KEY2, API_KEY3, API_KEY4 } = process.env;
 const URL = `https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&`;
 /*
 {
@@ -8,14 +8,14 @@ const URL = `https://api.spoonacular.com/recipes/complexSearch?addRecipeInformat
   "message": "You are not authorized. Please read https://spoonacular.com/food-api/docs#Authentication"
 }
 */
-const fetchAllData = async (number = 1) => {
+const fetchAllData = async (number = 100) => {
 
   console.log(`number of request ${number}`);
   let response = await fetch(`${URL}number=${number}&apiKey=${API_KEY}`);
-  if(response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY2}`);
-  if(response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY3}`);
-  if(response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY4}`);
-  if(response.status !== 200) throw new Error("Has consumed all 4 API KEYS"+response.status);
+  if (response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY2}`);
+  if (response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY3}`);
+  if (response.status !== 200) response = await fetch(`${URL}number=${number}&apiKey=${API_KEY4}`);
+  if (response.status !== 200) throw new Error("Has consumed all 4 API KEYS" + response.status);
   return response;
 }
 
